561472b5a981dc527fe54dbfed3e5031db724329
Cupid Valentine <westerngod@126.com>
2022-06-16 22:27:41 +0800

    format eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,13 @@ module.exports = {
     browser: true,
     es2021: true
   },
-  extends: ['plugin:react/recommended', 'standard', 'plugin:storybook/recommended'],
+  extends: [
+    'plugin:react/recommended',
+    'standard',
+    'plugin:storybook/recommended',
+    'plugin:storybook/recommended',
+    'plugin:storybook/recommended'
+  ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
