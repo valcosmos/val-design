3f19403e3925d8f864bd7594252aaaecf5ff5447
Cupid Valentine <westerngod@126.com>
2022-09-13 23:49:22 +0800

    feat: init form component

---
+++ src/components/Form/formItem.tsx
@@ -0,0 +1,24 @@
+import classNames from 'classnames'
+import React, { FC, ReactNode } from 'react'
+
+export interface FormItemProps {
+  label?: string
+  children?: ReactNode
+}
+
+export const FormItem: FC<FormItemProps> = (props) => {
+  const { label, children } = props
+  const rowClass = classNames('v-row', { 'v-row-no-label': !label })
+  return (
+    <div className={rowClass}>
+      {label && (
+        <div className="v-form-item-label">
+          <label title={label}>{label}</label>
+        </div>
+      )}
+      <div className="v-form-item">{children}</div>
+    </div>
+  )
+}
+
+export default FormItem
