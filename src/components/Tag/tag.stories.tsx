6de983b5408dd9a8088f7255db8a481248276180
Cupid Valentine <53572196+valcosmos@users.noreply.github.com>
2022-09-29 18:06:24 +0800

    feat: add tag component

---
+++ src/components/Tag/tag.stories.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { ComponentMeta, ComponentStory } from '@storybook/react'
+import Tag from './tag'
+
+const Meta: ComponentMeta<typeof Tag> = {
+  title: 'Data Display/Tag',
+  component: Tag
+}
+
+export default Meta
+
+const Template: ComponentStory<typeof Tag> = (args) => <Tag {...args} />
+
+export const BasicTag = Template.bind({})
+
+BasicTag.args = {
+  children: 'Basic tag'
+}
+
+export const TagSize = Template.bind({})
+
+TagSize.args = {
+  children: 'Tag size',
+  size: 'large'
+}
+export const TagTheme = Template.bind({})
+
+TagTheme.args = {
+  children: 'Theme size',
+  theme: 'plain'
+}
---
+++ src/components/Tag/tag.tsx
@@ -0,0 +1,44 @@
+import classNames from 'classnames'
+import React, { FC, ReactNode } from 'react'
+
+type TagType = 'success' | 'warning' | 'danger' | 'info'
+type TagSize = 'large' | 'medium' | 'small' | 'mini'
+type TagTheme = 'dark' | 'light' | 'plain'
+
+interface TagProps {
+  type?: TagType
+  size?: TagSize
+  theme?: TagTheme
+  color?: string
+  hit?: boolean
+  children: ReactNode
+}
+
+export const Tag: FC<TagProps> = ({
+  type,
+  size,
+  theme,
+  color,
+  hit,
+  children
+}) => {
+  const classes = classNames('v-tag', [
+    type && `v-tag--${type}`,
+    size && `v-tag--${size}`,
+    theme && `v-tag--${theme}`,
+    hit && 'is-hit'
+  ])
+  return (
+    <span className={classes} style={{ color: color }}>
+      {children}
+    </span>
+  )
+}
+
+Tag.defaultProps = {
+  type: 'success',
+  theme: 'light',
+  size: 'medium'
+}
+
+export default Tag
