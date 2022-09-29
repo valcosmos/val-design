081a50e23cb18d1bc62ae06ebe6426f64eeff7fd
Cupid Valentine <53572196+valcosmos@users.noreply.github.com>
2022-09-29 10:51:48 +0800

    feat: add card component

---
+++ src/components/Card/card.stories.tsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import { ComponentMeta, ComponentStory } from '@storybook/react'
+
+import Card from './card'
+
+const Meta: ComponentMeta<typeof Card> = {
+  title: 'val-design/Card 组件',
+  id: 'card',
+  component: Card,
+  decorators: [
+    (Story) => (
+      <div style={{ padding: '20px' }}>
+        <Story />
+      </div>
+    )
+  ]
+}
+
+export default Meta
+
+const Template: ComponentStory<typeof Card> = (args) => <Card {...args} />
+
+export const BasicCard = Template.bind({})
+BasicCard.args = {
+  header: 'Card header',
+  children: 'Card body'
+}
+
+export const BasicCardWithElement = Template.bind({})
+BasicCardWithElement.args = {
+  header: <h2>Card header</h2>,
+  children: <span>Card body</span>
+}
+
+export const ShadowCard = Template.bind({})
+ShadowCard.args = {
+  header: 'Card header',
+  shadow: 'always',
+  children: 'Card body'
+}
+
+export const CustomBodyStyle = Template.bind({})
+
+CustomBodyStyle.args = {
+  header: 'Card header',
+  shadow: 'always',
+  bodyStyle: { color: 'turquoise' },
+  children: 'Card body'
+}
