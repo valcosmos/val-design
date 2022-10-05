6c700c51f9fa87830de97c2df3c0cda19313de16
Cupid Valentine <westerngod@126.com>
2022-10-05 23:49:21 +0800

    fixed

--- scripts/add.js
+++
@@ -1,9 +0,0 @@
-// const path = require('path')
-// const { spawn } = require('child_process')
-import path from 'path'
-import { spawn } from 'child_process'
-
-;(() => {
-  const component = process.argv[2]
-  spawn('mkdir', ['-p', path.join(process.cwd(), `src/${component}`)])
-})()
