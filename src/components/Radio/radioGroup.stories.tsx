b285eb7f9e435d53c95fde5ddb6723511721d1b4
Cupid Valentine <53572196+valcosmos@users.noreply.github.com>
2022-10-12 23:21:06 +0800

    feat: add radio group component

---
+++ src/components/Radio/radioGroup.stories.tsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import { ComponentMeta, ComponentStory } from '@storybook/react'
+
+import RadioGroup from './radioGroup'
+import Radio from './radio'
+
+const RadioMeta: ComponentMeta<typeof RadioGroup> = {
+  title: 'RadioGroup 单选',
+  component: RadioGroup
+}
+
+export default RadioMeta
+
+const Template: ComponentStory<typeof RadioGroup> = (args) => (
+  <RadioGroup {...args} />
+)
+
+export const Primary = Template.bind({})
+Primary.args = {
+  children: [
+    <Radio value="1" key="1">
+      选项1
+    </Radio>,
+    <Radio value="2" key="2">
+      选项2
+    </Radio>,
+    <Radio value="3" key="3">
+      选项3
+    </Radio>
+  ]
+}
+
+export const UnderControl = () => {
+  return (
+    <RadioGroup value="3">
+      <Radio value="1" key="1">
+        选项1
+      </Radio>
+      <Radio value="2" key="2">
+        选项2
+      </Radio>
+      <Radio value="3" key="3">
+        选项3
+      </Radio>
+      ,
+    </RadioGroup>
+  )
+}
---
+++ src/components/Radio/radioGroup.tsx
@@ -0,0 +1,49 @@
+import classNames from 'classnames'
+import React, {
+  Children,
+  cloneElement,
+  CSSProperties,
+  FC,
+  FormEvent,
+  HTMLAttributes,
+  ReactNode,
+  useState
+} from 'react'
+import Radio from './radio'
+
+export interface RadioGroupProps extends HTMLAttributes<HTMLInputElement> {
+  value?: string
+  defaultValue?: string
+  onChange?: (event: FormEvent<HTMLInputElement>) => void
+  disabled?: boolean
+  className?: string
+  children?: ReactNode
+  style?: CSSProperties
+}
+
+export const RadioGroup: FC<RadioGroupProps> = (props) => {
+  const { disabled, children } = props
+
+  const [value, setValue] = useState(props.defaultValue || props.value)
+  const cls = classNames({
+    'v-radio-group': true
+  })
+
+  const handleClick = (e: any) => {
+    const value = e.target.value
+    setValue(value)
+  }
+
+  const newChildren = Children.map(children, (child: any) => {
+    if (child.type !== Radio) return null
+    return cloneElement(child, {
+      checked: child.props.value === value,
+      disabled: disabled,
+      onChange: handleClick
+    })
+  })
+
+  return <span className={cls}>{newChildren}</span>
+}
+
+export default RadioGroup
