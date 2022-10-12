77ff16bbb9f8b2b6f9b9017841ac2fc6f17dfdf5
valcosmos <valcosmos@users.noreply.github.com>
2022-10-12 15:23:16 +0000

    Deploying to gh-pages from @ valcosmos/val-design@229ce8b3a1e07da9269701102e4ccb37cc6e0eb4 🚀

---
+++ assets/icon.stories.c0bbb73c.js
@@ -0,0 +1,68 @@
+var i=Object.defineProperty;var e=(o,t)=>i(o,"name",{value:t,configurable:!0});import{I as c}from"./icon.fe25b1e4.js";import{j as r}from"./jsx-runtime.62764de5.js";import"./index.220dfe0f.js";import"./index.241e153c.js";import"./index.6f8f5b77.js";import"./iframe.8d9cac8e.js";const T={parameters:{storySource:{source:`import React from 'react'
+import { ComponentStory, ComponentMeta } from '@storybook/react'
+import Icon from './icon'
+
+export default {
+  title: 'General/Icon \u56FE\u6807',
+  component: Icon
+} as ComponentMeta<typeof Icon>
+
+const Template: ComponentStory<typeof Icon> = (props) => (
+  <Icon {...props} size="3x" />
+)
+
+export const CheckIcon = Template.bind({})
+CheckIcon.args = {
+  icon: 'check'
+}
+export const CoffeeIcon = Template.bind({})
+CoffeeIcon.args = {
+  icon: 'coffee'
+}
+
+export const CloseIcon = Template.bind({})
+CloseIcon.args = {
+  icon: 'xmark'
+}
+
+export const TrashIcon = Template.bind({})
+TrashIcon.args = {
+  icon: 'trash'
+}
+
+export const SuccessIcon = Template.bind({})
+SuccessIcon.args = {
+  icon: 'check',
+  theme: 'success'
+}
+export const DangerIcon = Template.bind({})
+DangerIcon.args = {
+  icon: 'times',
+  theme: 'danger'
+}
+export const PrimaryIcon = Template.bind({})
+PrimaryIcon.args = {
+  icon: 'anchor',
+  theme: 'primary'
+}
+export const WarningIcon = Template.bind({})
+WarningIcon.args = {
+  icon: 'exclamation-circle',
+  theme: 'warning'
+}
+
+export const Spin = Template.bind({})
+Spin.args = {
+  spin: true,
+  icon: 'spinner',
+
+  theme: 'primary'
+}
+export const Pulse = Template.bind({})
+Pulse.args = {
+  pulse: true,
+  icon: 'spinner',
+  theme: 'success'
+}
+`,locationsMap:{"check-icon":{startLoc:{col:46,line:10},endLoc:{col:1,line:12},startBody:{col:46,line:10},endBody:{col:1,line:12}},"coffee-icon":{startLoc:{col:46,line:10},endLoc:{col:1,line:12},startBody:{col:46,line:10},endBody:{col:1,line:12}},"close-icon":{startLoc:{col:46,line:10},endLoc:{col:1,line:12},startBody:{col:46,line:10},endBody:{col:1,line:12}},"trash-icon":{startLoc:{col:46,line:10},endLoc:{col:1,line:12},startBody:{col:46,line:10},endBody:{col:1,line:12}},"success-icon":{startLoc:{col:46,line:10},endLoc:{col:1,line:12},startBody:{col:46,line:10},endBody:{col:1,line:12}},"danger-icon":{startLoc:{col:46,line:10},endLoc:{col:1,line:12},startBody:{col:46,line:10},endBody:{col:1,line:12}},"primary-icon":{startLoc:{col:46,line:10},endLoc:{col:1,line:12},startBody:{col:46,line:10},endBody:{col:1,line:12}},"warning-icon":{startLoc:{col:46,line:10},endLoc:{col:1,line:12},startBody:{col:46,line:10},endBody:{col:1,line:12}},spin:{startLoc:{col:46,line:10},endLoc:{col:1,line:12},startBody:{col:46,line:10},endBody:{col:1,line:12}},pulse:{startLoc:{col:46,line:10},endLoc:{col:1,line:12},startBody:{col:46,line:10},endBody:{col:1,line:12}}}}},title:"General/Icon \u56FE\u6807",component:c},n=e(o=>r(c,{...o,size:"3x"}),"Template"),s=n.bind({});s.args={icon:"check"};const l=n.bind({});l.args={icon:"coffee"};const a=n.bind({});a.args={icon:"xmark"};const d=n.bind({});d.args={icon:"trash"};const p=n.bind({});p.args={icon:"check",theme:"success"};const m=n.bind({});m.args={icon:"times",theme:"danger"};const I=n.bind({});I.args={icon:"anchor",theme:"primary"};const y=n.bind({});y.args={icon:"exclamation-circle",theme:"warning"};const g=n.bind({});g.args={spin:!0,icon:"spinner",theme:"primary"};const h=n.bind({});h.args={pulse:!0,icon:"spinner",theme:"success"};const k=["CheckIcon","CoffeeIcon","CloseIcon","TrashIcon","SuccessIcon","DangerIcon","PrimaryIcon","WarningIcon","Spin","Pulse"];export{s as CheckIcon,a as CloseIcon,l as CoffeeIcon,m as DangerIcon,I as PrimaryIcon,h as Pulse,g as Spin,p as SuccessIcon,d as TrashIcon,y as WarningIcon,k as __namedExportsOrder,T as default};
+//# sourceMappingURL=icon.stories.c0bbb73c.js.map
