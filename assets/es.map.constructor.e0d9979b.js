0e9d6e56043c83a8cc5ca81105ce9ab90e611794
valcosmos <valcosmos@users.noreply.github.com>
2022-11-21 15:02:30 +0000

    Deploying to gh-pages from @ valcosmos/val-design@3e08777f28e1767d74d1223bde591cb6703609a8 🚀

--- assets/es.map.constructor.e0d9979b.js
+++
@@ -1,2 +0,0 @@
-var r=Object.defineProperty;var t=(n,o)=>r(n,"name",{value:o,configurable:!0});import{n as e,o as c}from"./iframe.e7600c43.js";var i=e,l=c;i("Map",function(n){return t(function(){return n(this,arguments.length?arguments[0]:void 0)},"Map")},l);
-//# sourceMappingURL=es.map.constructor.e0d9979b.js.map
---
+++ assets/progress.stories.e48d166a.js
@@ -0,0 +1,26 @@
+var s=Object.defineProperty;var e=(o,t)=>s(o,"name",{value:t,configurable:!0});import{P as r}from"./progress.5847cc0c.js";import{j as n}from"./jsx-runtime.a474e9ed.js";import"./iframe.2d80ae98.js";const g={parameters:{storySource:{source:`import { ComponentStory, ComponentMeta } from '@storybook/react'
+import React from 'react'
+
+import { Progress } from './progress'
+
+export default {
+  /* \u{1F447} The title prop is optional.
+   * See https://storybook.js.org/docs/react/configure/overview#configure-story-loading
+   * to learn how to generate automatic titles
+   */
+  title: 'Feedback/Progress \u8FDB\u5EA6\u6761',
+  component: Progress
+} as ComponentMeta<typeof Progress>
+
+const Template: ComponentStory<typeof Progress> = (args) => (
+  <Progress {...args} />
+)
+
+export const DefaultProgress = Template.bind({})
+
+DefaultProgress.args = {
+  percent: 80,
+  theme: 'primary'
+}
+`,locationsMap:{"default-progress":{startLoc:{col:50,line:15},endLoc:{col:1,line:17},startBody:{col:50,line:15},endBody:{col:1,line:17}}}}},title:"Feedback/Progress \u8FDB\u5EA6\u6761",component:r},a=e(o=>n(r,{...o}),"Template"),p=a.bind({});p.args={percent:80,theme:"primary"};const f=["DefaultProgress"];export{p as DefaultProgress,f as __namedExportsOrder,g as default};
+//# sourceMappingURL=progress.stories.e48d166a.js.map
