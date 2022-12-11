ea9f6d9de3974fd8e7b729bc2ff17e43ea51055e
Cupid Valentine <westerngod@126.com>
2022-12-11 11:22:59 +0800

    chore: update vitest configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,7 @@
-/// <reference types="vitest" />
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  test: {
-    globals: true,
-    environment: 'happy-dom'
-  }
+  plugins: [react()]
 })
