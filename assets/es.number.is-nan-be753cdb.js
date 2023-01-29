b073d2907a9a68c5f62a24de6c5da0aeb027ab22
valcosmos <valcosmos@users.noreply.github.com>
2023-01-29 15:36:52 +0000

    Deploying to gh-pages from @ valcosmos/val-design@4bc98d5877c3a5126ee3e5bb284945e01fade467 🚀

--- assets/es.number.is-nan-be753cdb.js
+++
@@ -1,2 +0,0 @@
-var N=Object.defineProperty;var a=(r,t)=>N(r,"name",{value:t,configurable:!0});import{_ as e}from"./es.object.get-own-property-descriptor-fbf95828.js";var i=e;i({target:"Number",stat:!0},{isNaN:a(function(t){return t!=t},"isNaN")});
-//# sourceMappingURL=es.number.is-nan-be753cdb.js.map
---
+++ assets/input-f932e6ca.js
@@ -0,0 +1,18 @@
+var F=Object.defineProperty;var i=(u,n)=>F(u,"name",{value:n,configurable:!0});import{c as E}from"./index-ed86a6de.js";import{I as m}from"./icon-938a876b.js";import{a as c,j as e}from"./jsx-runtime-69008c6b.js";const r=i(u=>{const{disabled:n,size:l,icon:p,prepend:a,append:t,style:s,...d}=u,o=E("v-input-wrapper",{[`input-size-${l}`]:l,"is-disabled":n,"input-group":a||t,"input-group-append":!!t,"input-group-prepend":!!a});return c("div",{className:o,style:s,children:[a&&e("div",{className:"v-input-group-prepend",children:a}),p&&e("div",{className:"icon-wrapper",children:e(m,{icon:p,title:`title-${p}`})}),e("input",{className:"v-input-inner",disabled:n,...d}),t&&e("div",{className:"v-input-group-append",children:t})]})},"Input");try{r.displayName="Input",r.__docgenInfo={description:`> 一个基础的Input组件封装，可添加图标，前缀或者后缀组合。
+
+### 引用方法
+
+\`\`\`js
+
+import { Input } from 'val-design'
+
+\`\`\``,displayName:"Input",props:{disabled:{defaultValue:null,description:"是否禁用 Input",name:"disabled",required:!1,type:{name:"boolean"}},size:{defaultValue:null,description:"设置 input 大小，支持 lg 或者是 sm",name:"size",required:!1,type:{name:"enum",value:[{value:'"lg"'},{value:'"sm"'}]}},icon:{defaultValue:null,description:"添加图标，在右侧悬浮添加一个图标，用于提示",name:"icon",required:!1,type:{name:"IconProp"}},prepend:{defaultValue:null,description:"添加前缀 用于配置一些固定组合",name:"prepend",required:!1,type:{name:"string | ReactElement<any, string | JSXElementConstructor<any>>"}},append:{defaultValue:null,description:"添加后缀 用于配置一些固定组合",name:"append",required:!1,type:{name:"string | ReactElement<any, string | JSXElementConstructor<any>>"}},onChange:{defaultValue:null,description:"",name:"onChange",required:!1,type:{name:"((e: ChangeEvent<HTMLInputElement>) => void)"}}}}}catch{}try{input.displayName="input",input.__docgenInfo={description:`> 一个基础的Input组件封装，可添加图标，前缀或者后缀组合。
+
+### 引用方法
+
+\`\`\`js
+
+import { Input } from 'val-design'
+
+\`\`\``,displayName:"input",props:{disabled:{defaultValue:null,description:"是否禁用 Input",name:"disabled",required:!1,type:{name:"boolean"}},size:{defaultValue:null,description:"设置 input 大小，支持 lg 或者是 sm",name:"size",required:!1,type:{name:"enum",value:[{value:'"lg"'},{value:'"sm"'}]}},icon:{defaultValue:null,description:"添加图标，在右侧悬浮添加一个图标，用于提示",name:"icon",required:!1,type:{name:"IconProp"}},prepend:{defaultValue:null,description:"添加前缀 用于配置一些固定组合",name:"prepend",required:!1,type:{name:"string | ReactElement<any, string | JSXElementConstructor<any>>"}},append:{defaultValue:null,description:"添加后缀 用于配置一些固定组合",name:"append",required:!1,type:{name:"string | ReactElement<any, string | JSXElementConstructor<any>>"}},onChange:{defaultValue:null,description:"",name:"onChange",required:!1,type:{name:"((e: ChangeEvent<HTMLInputElement>) => void)"}}}}}catch{}export{r as I};
+//# sourceMappingURL=input-f932e6ca.js.map
