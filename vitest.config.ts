c680e70d1af0c228a53a183e334ea2191be34f8f
Cupid Valentine <53572196+valcosmos@users.noreply.github.com>
2023-01-31 15:04:25 +0800

    ci: fixed the error in vitest config file

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     globals: true,
-    environment: 'happy-dom'
+    environment: 'jsdom',
+    setupFiles: ['./vitest-setup.ts']
   }
 })
