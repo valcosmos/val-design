6962e19ce9af9a258ef5b6ad0ce5fcd5258c9358
Cupid Valentine <53572196+valcosmos@users.noreply.github.com>
2023-01-31 14:57:13 +0800

    test: finished button test

--- src/components/Button/button.test.tsx
+++ src/components/Button/button.test.tsx
@@ -1,28 +1,53 @@
-import { render } from '@testing-library/react'
-import React from 'react'
+import { fireEvent, render } from '@testing-library/react'
 import Button from './index'
 
 test('Button component should work', () => {
-  const { getByText } = render(<Button>button</Button>)
+  const onButtonClick = vi.fn()
+  const { getByText } = render(<Button onClick={onButtonClick}>button</Button>)
   const element = getByText('button')
   expect(element).toBeTruthy()
+  expect(element).toBeInTheDocument()
   expect(element.tagName).toEqual('BUTTON')
   expect(element.className).toEqual('btn btn-default')
+  fireEvent.click(element)
+  expect(onButtonClick).toHaveBeenCalled()
 })
 
-test('Button type should work', () => {
-  const { getByRole } = render(<Button type="primary"></Button>)
+test('Button type and custom class should work', () => {
+  const { getByRole } = render(<Button type="primary" className="my-button"></Button>)
   const button = getByRole('button')
-  expect(button.classList.contains('btn-primary'))
+  expect(button).toBeInTheDocument()
+  expect(button).toHaveClass('btn-primary my-button')
+})
+
+test('Link button should work', () => {
+  const { getByText } = render(
+    <Button type={'link'} href="https://www.myurl.com/">
+      link
+    </Button>
+  )
+  const link = getByText('link') as HTMLLinkElement
+  expect(link).toBeInTheDocument()
+  expect(link.tagName).toEqual('A')
+  expect(link).toHaveClass('btn btn-link')
+  expect(link.href).toEqual('https://www.myurl.com/')
 })
 
 test('Button size should work', () => {
   const { getByRole } = render(<Button size="sm" />)
   const button = getByRole('button')
-  expect(button.classList.contains('btn-sm'))
+  expect(button.className.includes('btn-sm')).toBe(true)
 })
 
-test('Button children should work', () => {
-  const { getByText } = render(<Button>Button</Button>)
-  getByText('Button')
+test('Disabled button should work', () => {
+  const onButtonClick = vi.fn()
+  const { getByRole } = render(
+    <Button disabled={true} onClick={onButtonClick}>
+      disabled
+    </Button>
+  )
+  const button = getByRole('button') as HTMLButtonElement
+  expect(button.disabled).toBe(true)
+  fireEvent.click(button)
+  expect(onButtonClick).not.toHaveBeenCalled()
 })
