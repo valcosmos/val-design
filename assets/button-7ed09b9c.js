873f79afbd4973ff9135d359442e929060c17178
valcosmos <53572196+valcosmos@users.noreply.github.com>
2023-08-21 15:36:20 +0000

    Deploying to gh-pages from @ valcosmos/val-design@4061341d4f404a956d55fa044f66983f1714d3d6 🚀

---
+++ assets/button-7ed09b9c.js
@@ -0,0 +1,14 @@
+import{j as t}from"./jsx-runtime-4ca860c5.js";import"./chunk-S4VUQJ4A-25946713.js";import{C as i,b as n,A as a}from"./index-2b40e5fc.js";import{B as m}from"./button-87ec6eb6.js";import{u as s}from"./index-e744116c.js";import"./index-61bf1805.js";import"./_commonjsHelpers-de833af9.js";import"./iframe-fe2c4163.js";import"../sb-preview/runtime.js";import"./react-18-b914d09f.js";import"./client-e8a49af7.js";import"./index-a199a0ff.js";import"./index-d475d2ea.js";import"./index-d37d4223.js";import"./inheritsLoose-9eefaa95.js";import"./assertThisInitialized-081f9914.js";import"./index-afe93f9d.js";import"./index-356e4a49.js";import"./index-e131923d.js";function o(e){const r=Object.assign({h3:"h3"},s(),e.components);return t.jsxs(t.Fragment,{children:[t.jsx("style",{children:`
+.title{
+  margin-bottom: 40px;
+}
+
+`}),`
+`,t.jsx("h1",{className:"title",children:"Button 按钮"}),`
+`,t.jsx(r.h3,{id:"不同-type-的按钮",children:"不同 Type 的按钮"}),`
+`,t.jsxs(i,{children:[t.jsx(n,{id:"general-button-按钮--default"}),t.jsx(n,{id:"general-button-按钮--primary"}),t.jsx(n,{id:"general-button-按钮--success"}),t.jsx(n,{id:"general-button-按钮--warning"}),t.jsx(n,{id:"general-button-按钮--danger"}),t.jsx(n,{id:"general-button-按钮--link"})]}),`
+`,t.jsx(r.h3,{id:"不同-size-的按钮",children:"不同 Size 的按钮"}),`
+`,t.jsxs(i,{children:[t.jsx(n,{id:"general-button-按钮--large-button"}),t.jsx(n,{id:"general-button-按钮--small-button"})]}),`
+`,t.jsx(r.h3,{id:"文档",children:"文档"}),`
+`,t.jsx(a,{of:m})]})}function z(e={}){const{wrapper:r}=Object.assign({},s(),e.components);return r?t.jsx(r,Object.assign({},e,{children:t.jsx(o,e)})):o(e)}export{z as default};
+//# sourceMappingURL=button-7ed09b9c.js.map
